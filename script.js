fbfb6df7b697b7735bd8555f107d678f0bdc002a
Ruturaj Nikam <ruturajnikam1010@gmail.com>
2024-08-18 16:29:39 +0530

    Made navbar sticky and build about section

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-console.log("this is portfolio website")
-console.log('19:10');
+console.log( "this is portfolio website" )
+console.log( '22:41' );
+
+var tablinks = document.getElementsByClassName( "tab-links" )
+var tabcontents = document.getElementsByClassName( "tab-contents" )
+
+function opentab (tabname ) {
+    for ( tablink of tablinks ) {
+        tablink.classList.remove( "active-link" );
+    }
+
+    for ( tabcontent of tabcontents ) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
