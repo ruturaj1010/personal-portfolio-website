aaf26b09b6ea469f8564a1e335cc8b1b9d43f755
Ruturaj Nikam <ruturajnikam1010@gmail.com>
2024-08-21 19:26:54 +0530

    completed website with adding Responsiveness

--- script.js
+++ script.js
@@ -4,15 +4,24 @@ console.log( '1:21:41' );
 var tablinks = document.getElementsByClassName( "tab-links" )
 var tabcontents = document.getElementsByClassName( "tab-contents" )
 
-function opentab (tabname ) {
+function opentab ( tabname ) {
     for ( tablink of tablinks ) {
         tablink.classList.remove( "active-link" );
     }
 
     for ( tabcontent of tabcontents ) {
-        tabcontent.classList.remove("active-tab");
+        tabcontent.classList.remove( "active-tab" );
     }
 
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
+    event.currentTarget.classList.add( "active-link" );
+    document.getElementById( tabname ).classList.add( "active-tab" );
 }
+
+document.getElementById( "close" ).addEventListener( "click", () => {
+    document.querySelector( "nav ul" ).style.right = -100 + "%";
+    document.querySelector( "nav ul" ).style.transition = 1 + "s";
+} )
+
+document.getElementById( "open" ).addEventListener( "click", () => {
+    document.querySelector( "nav ul" ).style.right = 0;
+} )
