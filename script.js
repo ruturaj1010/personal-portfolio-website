ebc6f3fbe6bd146774caffddb94b6593057bf71f
Ruturaj Nikam <ruturajnikam1010@gmail.com>
2024-08-25 02:22:04 +0530

    changed js code for tab toggling in about section

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 console.log( "this is portfolio website" )
 console.log( '1:21:41' );
 
-var tablinks = document.getElementsByClassName( "tab-links" )
-var tabcontents = document.getElementsByClassName( "tab-contents" )
+// var tablinks = document.getElementsByClassName( "tab-links" )
+// var tabcontents = document.getElementsByClassName( "tab-contents" )
 
-function opentab ( tabname ) {
-    for ( tablink of tablinks ) {
-        tablink.classList.remove( "active-link" );
-    }
+// function opentab ( tabname , event) {
+//     for ( tablink of tablinks ) {
+//         tablink.classList.remove( "active-link" );
+//     }
 
-    for ( tabcontent of tabcontents ) {
-        tabcontent.classList.remove( "active-tab" );
-    }
+//     for ( tabcontent of tabcontents ) {
+//         tabcontent.classList.remove( "active-tab" );
+//     }
 
-    event.currentTarget.classList.add( "active-link" );
-    document.getElementById( tabname ).classList.add( "active-tab" );
-}
+//     event.currentTarget.classList.add( "active-link" );
+//     document.getElementById( tabname ).classList.add( "active-tab" );
+// }
 
 document.getElementById( "close" ).addEventListener( "click", () => {
     document.querySelector( "nav ul" ).style.right = -100 + "%";
@@ -25,3 +25,20 @@ document.getElementById( "close" ).addEventListener( "click", () => {
 document.getElementById( "open" ).addEventListener( "click", () => {
     document.querySelector( "nav ul" ).style.right = 0;
 } )
+
+
+let tablinks = Array.from(document.getElementsByClassName("tab-links"));
+let tabconts = Array.from(document.getElementsByClassName("tab-contents"));
+
+tablinks.forEach(tablink => {
+    tablink.addEventListener("click", e => {
+        
+        tablinks.forEach(link => link.classList.remove("active-link"));
+        tabconts.forEach(content => content.classList.remove("active-tab"));
+
+        e.currentTarget.classList.add("active-link");
+
+        const tabIndex = tablinks.indexOf(e.currentTarget);
+        tabconts[tabIndex].classList.add("active-tab");
+    });
+});
